530e3a2f35efd8260bbe4064d7516cfe11a72198
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-05-28 13:13:26 -0500

    Rutas de los componentes declarada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JokesComponent } from './components/jokes/jokes.component';
+import { PoemsComponent } from './components/poems/poems.component';
+import { SongComponent } from './components/song/song.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { IndexComponent } from './components/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: IndexComponent},
+  {path:"song", component: SongComponent},
+  {path:"jokes", component: JokesComponent},
+  {path:"poems", component: PoemsComponent},
+  {path:"404", component: NotFoundComponent},
+  {path:"**", redirectTo: "404"},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
