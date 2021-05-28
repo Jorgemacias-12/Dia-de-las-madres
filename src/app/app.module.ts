9f5e64510fd5f0e3310d74827ef9cc2fc6abe2ae
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-05-28 00:04:25 -0500

    Se han añadido las declaraciones de los componentes generados a este archivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { SongComponent } from './components/song/song.component';
+import { JokesComponent } from './components/jokes/jokes.component';
+import { PoemsComponent } from './components/poems/poems.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    SongComponent,
+    JokesComponent,
+    PoemsComponent
   ],
   imports: [
     BrowserModule,
