89e161fd41eed4fa113250c6c2ee0a5eeb8b04cf
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-06-04 00:35:36 -0500

    Se ha generado, he implementado el componente de carrousel en el componente index ademas de estar funcionando

---
+++ src/app/components/carrousel/carrousel.component.ts
@@ -0,0 +1,64 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carrousel',
+  templateUrl: './carrousel.component.html',
+  styleUrls: ['./carrousel.component.css'],
+  animations: [
+    trigger('controls', [
+      state('inactive', style({
+        opacity: 0,
+      })),
+      state('active', style({
+        opacity: 1,
+      })),
+      transition('inactive => active', animate('0.5s')),
+      transition('active => inactive', animate('0.3s'))
+    ])
+  ]
+})
+export class CarrouselComponent implements OnInit {
+
+  index: number = 0;
+
+  state: string = 'false';
+
+  paths = [
+    "../../../assets/1.jpg",
+    "../../../assets/2.jpg",
+    "../../../assets/3.jpg",
+    "../../../assets/4.jpg",
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  activateControls(value: string) {
+    this.state = value;
+  }
+
+  counter: number = 0;
+
+  prevImage() {
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.paths.length - 1;
+    }
+    let imageElement = document.getElementById("image") as HTMLImageElement;
+    imageElement.src = this.paths[this.index];
+  }
+
+  nextImage() {
+    this.index++;
+    if (this.index >= this.paths.length) {
+      this.index = 0;
+    }
+    let imageElement = document.getElementById("image") as HTMLImageElement;
+    imageElement.src = this.paths[this.index];
+  }
+
+
+}
