3e143f1590243c7ea6ad345824526bd601b4bfcf
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-06-05 18:42:49 -0500

    Añadiendo animacion a la rutas de la pagina.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { IndexComponent } from './components/index/index.component';
 
 const routes: Routes = [
-  {path:"", component: IndexComponent},
-  {path:"song", component: SongComponent},
-  {path:"jokes", component: JokesComponent},
-  {path:"poems", component: PoemsComponent},
+  {path:"", component: IndexComponent, data: {animation: 'HomePage'}},
+  {path:"song", component: SongComponent, data: {animation: 'SongPage'}},
+  {path:"jokes", component: JokesComponent, data: {animation: 'JokePage'}},
+  {path:"poems", component: PoemsComponent, data: {animation: 'PoemPage'}},
   {path:"404", component: NotFoundComponent},
   {path:"**", redirectTo: "404"},
 ];
