a29c2fee984d4ce29d6fcc61435f77c6e3be83eb
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-06-13 23:22:24 -0500

    Añadiendo estilos responsivos al componente, y modificando el acceso a los estilos del componente

--- src/app/components/jokes/jokes.component.ts
+++ src/app/components/jokes/jokes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jokes',
   templateUrl: './jokes.component.html',
-  styleUrls: ['./jokes.component.css']
+  styleUrls: ['./jokes.component.css', './jokes.responsive.css']
 })
 export class JokesComponent implements OnInit {
 
