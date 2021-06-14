c61196fb458e7824408d34500b869b28bf841fc9
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2021-06-13 22:42:24 -0500

    Archivo de estilos responsivos generados y ruta de estilos modificada para el componente

--- src/app/components/song/song.component.ts
+++ src/app/components/song/song.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-song',
   templateUrl: './song.component.html',
-  styleUrls: ['./song.component.css']
+  styleUrls: ['./song.component.css', './song.responsive.css']
 })
 export class SongComponent implements OnInit {
 
